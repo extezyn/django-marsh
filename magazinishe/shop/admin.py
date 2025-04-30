a67da557e71819ec8e14c2b2193488d1b8e76ea0
extezyn <144113053+extezyn@users.noreply.github.com>
2025-04-30 09:53:45 +0300

    2nd commit

--- magazinishe/shop/admin.py
+++ magazinishe/shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import * 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Clothes)
+class ClothesCollection(admin.ModelAdmin):
+    pass
--- magazinishe/shop/models.py
+++ magazinishe/shop/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+MAX_LENGTH = 255
+
+class Category(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH, verbose_name = 'Название')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Collection(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH, verbose_name = 'Название')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Коллекция'
+        verbose_name_plural = 'Коллекция'
+
+class Clothes(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH, verbose_name = 'Название')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+    price = models.FloatField(verbose_name='Цена')
+    color = models.CharField(max_length = MAX_LENGTH, verbose_name='Цвет')
+    min_size = models.PositiveIntegerField(default=36, verbose_name='Минимальный размер')
+    max_size = models.PositiveIntegerField(default=52, verbose_name='Максимальный размер')
+    photo = models.ImageField(upload_to='image/%Y/%m/%d', null=True, blank=True, verbose_name='изображение')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления на сайт')
+    is_exist = models.BooleanField(default=True, verbose_name='В наличии')
+
+    collection = models.ManyToManyField(Collection, verbose_name='Коллекция')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Категория')
+    
+
+    def __str__(self):
+        return f'{self.name} - {self.price} руб.'
+    
+    class Meta:
+        verbose_name = 'Одежда'
+        verbose_name_plural = 'Одежда'
